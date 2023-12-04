32e2599b554945ea18a41207626535d34ee4ce40
William Y. Feng <womogenes2@gmail.com>
2023-12-03 17:53:24 -0800

    More modal tweaking

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -21,11 +21,15 @@
 
 {#if show}
   <div
-    class="absolute z-20 flex h-full w-full items-start justify-center bg-neutral-500 bg-opacity-50 pt-40"
+    class="absolute z-20 flex h-full w-full items-start justify-center bg-white bg-opacity-50 pt-40"
     transition:fade={{ duration: 100 }}
   >
-    <div class="flex w-full max-w-sm flex-col gap-4 border bg-white px-6 py-4">
-      <h1>{$curModal?.title}</h1>
+    <div
+      class="relative flex w-full max-w-sm flex-col gap-2 border-2 bg-white px-6 py-6 shadow-md shadow-neutral-400"
+    >
+      <h1 class="absolute -top-2 left-4 bg-white px-2 font-bold leading-none">
+        {$curModal?.title}
+      </h1>
       <div class="flex gap-4">
         {#each $curModal?.actions || [] as action}
           <button
